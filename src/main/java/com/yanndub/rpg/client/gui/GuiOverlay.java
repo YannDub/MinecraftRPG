98dfc10160f42bcefedb75eaae5a61b8968cac61
Yann Dubois <yann59.dubois@gmail.com>
2017-06-15 19:50:32 +0200

    Change color of money

--- src/main/java/com/yanndub/rpg/client/gui/GuiOverlay.java
+++ src/main/java/com/yanndub/rpg/client/gui/GuiOverlay.java
@@ -1,19 +1,26 @@
 package com.yanndub.rpg.client.gui;
 
+import org.lwjgl.opengl.GL11;
+
 import com.yanndub.rpg.capabilities.CapabilityHandler;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.util.ResourceLocation;
 
 public class GuiOverlay extends Gui {
+	
+	private ResourceLocation dollarsTexture = new ResourceLocation("minecraftrpg", "textures/gui/dollars.png");
 	
 	public GuiOverlay(ScaledResolution scaled) {
 		super();
 		Minecraft mc = Minecraft.getMinecraft();
 		int width = scaled.getScaledWidth();
+		int height = scaled.getScaledHeight();
+		
 		String currentMoney = "" + mc.thePlayer.getCapability(CapabilityHandler.MONEY_CAP, null).getMoney();
 		
-		mc.fontRendererObj.drawStringWithShadow(currentMoney, (width - currentMoney.length() * mc.fontRendererObj.FONT_HEIGHT), mc.fontRendererObj.FONT_HEIGHT, Integer.parseInt("FFFFFF", 16));
+		mc.fontRendererObj.drawStringWithShadow(currentMoney, (width - currentMoney.length() * mc.fontRendererObj.FONT_HEIGHT), mc.fontRendererObj.FONT_HEIGHT, Integer.parseInt("00FF00", 16));
 	}
 }
